149745ffcd27e62b115087791afe98d15935855d
Markus Lindenberg <markus@lindenberg.io>
2019-10-25 17:09:13 +0200

    Remove pam, use nginx

--- board/common/post-build.sh
+++ board/common/post-build.sh
@@ -3,7 +3,13 @@
 set -u
 set -e
 
-rm -f ${TARGET_DIR}/etc/pam.d/*
-cp -f ${TARGET_DIR}/usr/share/factory/etc/pam.d/* ${TARGET_DIR}/etc/pam.d/
-mkdir -p ${TARGET_DIR}/etc/dropbear
+if [ ! -d ${TARGET_DIR}/etc/dropbear ]; then
+	rm -f ${TARGET_DIR}/etc/dropbear
+	mkdir -p ${TARGET_DIR}/etc/dropbear
+fi
+
+rm -f ${TARGET_DIR}/etc/nginx/*.default
+rm -f ${TARGET_DIR}/etc/nginx/fastcgi*
+rm -f ${TARGET_DIR}/etc/nginx/scgi*
+rm -f ${TARGET_DIR}/etc/nginx/uwsgi*
 
