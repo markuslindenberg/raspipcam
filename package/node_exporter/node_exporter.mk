d44af9e76ec49e5b6aa2ee3752aaa4c8f1581333
Markus Lindenberg <markus@lindenberg.io>
2019-11-05 14:35:38 +0100

    Fix node_exporter systemd link

--- package/node_exporter/node_exporter.mk
+++ package/node_exporter/node_exporter.mk
@@ -12,9 +12,9 @@ NODE_EXPORTER_LICENSE_FILES = LICENSE
 define NODE_EXPORTER_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 644 $(NODE_EXPORTER_PKGDIR)/node_exporter.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/node_exporter.service
-	mkdir -p $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants
-	ln -sf ../node_exporter.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/node_exporter.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -sf ../../../../usr/lib/systemd/system/node_exporter.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/node_exporter.service
 endef
 
 $(eval $(golang-package))
